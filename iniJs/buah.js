2aa2ea2601593711e8d0a43a4b0fc643bebf7045
Ahmad Syakban <ahmadsyakban240@gmail.com>
2022-08-12 10:33:02 +0700

    Menghubungkan produk dengan keranjang

--- iniJs/buah.js
+++ iniJs/buah.js
@@ -1,10 +1,26 @@
 let cardContainer = document.getElementById("container-card")
+let cart = []
 
-console.log(cardContainer);
+let addToCart = (id) => {
+  let objectBuahLocalStorage = JSON.parse(localStorage.getItem("buah"))
+   // ngambil dari local storage dengan key sayur
+
+   // mencari obj dengan id yang sama
+   let foundBuah = objectBuahLocalStorage.find( buah => {
+    return buah.id === Number(id)
+  })
+  
+  cart.push(foundBuah)
+  localStorage.setItem("cart-buah", JSON.stringify(cart))
+}
 
 fetch('https://childish-jumpy-kitten.glitch.me/buah')
 .then((response) => response.json())
 .then((data) => {
+    // masukin ke local storage
+    localStorage.setItem('buah', JSON.stringify(data))
+
+
     console.log(data)
     data.forEach(element => {
         cardContainer.innerHTML += `
@@ -15,11 +31,20 @@ fetch('https://childish-jumpy-kitten.glitch.me/buah')
             <h5 class="card-title">${element.name}</h5>
             <p class="card-text">Harga : ${element.harga} (kg)</p>
             <p class="stok">Stok: ${element.stok}</p>
-            <a href="/iniHtml/keranjang.html" class="col-12 btn btn-success button">+Keranjang</a>
+            <button class="col-12 btn btn-success button btn-keranjang" id=${element.id}>+Keranjang</button>
             <a href="/iniHtml/checkout-transaksi.html" class="col-12 btn btn-success button">Beli Langsung</a>
             </div>
             </div>
         </div>
         `
     });
+
+    let button_carts = document.querySelectorAll('.btn-keranjang')
+    // loping pakek for each
+    
+    button_carts.forEach(button => {
+        button.addEventListener("click", (e) => addToCart(button.id))
+        // button_carts.preventDefault();
+    })
+
 });
