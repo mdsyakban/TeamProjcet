a975e7bc72550d87877eafea52b0281dcd2a068f
Pandji Setiya Budhi Artha <pandjisetiya@gmail.com>
2022-08-14 22:40:49 +0800

    total card

--- iniJs/keranjang.js
+++ iniJs/keranjang.js
@@ -41,7 +41,7 @@ cartSayur.forEach(sayur => {
                   </button>
                 </div>
                 <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1" id="value-${sayur.id}">
-                  <h5 class="mb-0">${sayur.harga}</h5>
+                  <h5 class="mb-0 total-rincianbarang">${sayur.harga}</h5>
                 </div>
                 <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                 <button type="button" class="btn btn-danger" id="buttonHapus"><i class="bi bi-trash-fill"></i></button>
@@ -50,6 +50,26 @@ cartSayur.forEach(sayur => {
             </div>
           </div>
   `
+  let plusbutton = document.querySelectorAll(`#plus`);
+  let minusbutton = document.querySelectorAll("#minus");
+  let totalHargaSayur = document.querySelectorAll(".total-rincianbarang");
+
+  plusbutton.forEach((element) => {
+    element.addEventListener("click", function (event) {
+      let quantityFind = document.querySelectorAll(`.total-rincianbarang`);
+      quantityFind.forEach((quantity) => {
+        if (quantity.name === element.name) {
+          quantity.value = Number(quantity.value) + 1;
+          totalHarga = [];
+        }
+      });
+      totalHargaSayur.forEach((total) => {
+        if (total.id.split("_")[0] === element.name) {
+          total.textContent = Number(total.textContent) + Number(total.id.split("_")[1]);
+        }
+      });
+    });
+  });
 
   //Hapus Local Storage
   let hapusButton = document.querySelector('#buttonHapus');
@@ -127,5 +147,40 @@ function refresh() {
   window.location.reload();
 }
 } 
-
 })
+
+
+let array = [];
+let array1 = [];
+let dataHarga = [];
+datakuantitiy = [];
+datakuantitiy1 = [];
+namanama = [];
+
+let jumlah = document.getElementById("jumlahbarangsemua");
+let total = document.getElementById("totalpembayaransemua");
+
+let jumlahSayur = parseInt(document.getElementById("jumlahbarang").innerHTML);
+let jumlahBuah = parseInt(document.getElementById("jumlahbarang1").innerHTML);
+
+let totalsayur = parseInt(document.getElementById("totalpembayaran").innerHTML);
+let totalbuah = parseInt(document.getElementById("totalpembayaran1").innerHTML);
+
+// console.log(totalbuah);
+
+jumlah.textContent = jumlahSayur + jumlahBuah;
+total.textContent = totalsayur + totalbuah;
+
+let tombol = document.getElementById("tombol");
+let jumlah1 = parseInt(document.getElementById("jumlahbarangsemua").innerHTML);
+let total1 = parseInt(document.getElementById("totalpembayaransemua").innerHTML);
+
+tombol.addEventListener("click", (e) => {
+  let TotalHarga = {
+    Key: "0",
+    harga: total1,
+    jumlah: jumlah1,
+  };
+  dataHarga.push(TotalHarga);
+  localStorage.setItem("totalharga", JSON.stringify(dataHarga));
+});
